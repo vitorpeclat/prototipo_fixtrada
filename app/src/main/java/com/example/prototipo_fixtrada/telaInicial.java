d46f70a7857044f856d736452403085ee233400f
pedrosantos01197 <pedro.santos01197@gmail.com>
2025-06-09 16:51:30 -0300

    Update telaInicial.java

--- app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
+++ app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class telaInicial extends AppCompatActivity {
-//teste
+//a
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
