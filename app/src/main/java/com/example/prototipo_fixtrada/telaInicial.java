7acacde154b61a318a1344e5bee79fd8bb8986f7
Vitor Rodrigues Peclat <vitorr.peclat@gmail.com>
2025-06-09 15:53:43 -0300

    nome

--- app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
+++ app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class InicialCliente extends AppCompatActivity {
+public class telaInicial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
