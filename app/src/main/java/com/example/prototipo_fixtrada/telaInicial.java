23cef4ec91691c839e200cb917493bcbf097f713
Vitor Rodrigues Peclat <vitorr.peclat@gmail.com>
2025-06-09 17:25:11 -0300

    base

--- app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
+++ app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
@@ -14,11 +14,5 @@ public class telaInicial extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.tela_inicial);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-            //caralho
-        });
     }
 }
