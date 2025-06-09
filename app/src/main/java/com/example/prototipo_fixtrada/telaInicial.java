3748a237574b507c85fb6a1a36719130962f613d
Vitor Rodrigues Peclat <vitorr.peclat@gmail.com>
2025-06-09 16:56:49 -0300

    teste

--- app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
+++ app/src/main/java/com/example/prototipo_fixtrada/telaInicial.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class telaInicial extends AppCompatActivity {
-//a
+//abdce
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
